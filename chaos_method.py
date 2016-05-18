7c0fe0ccaaae6ebb2ee64b04baa147a3d7f35498
scottgeraedts <scott.geraedts@gmail.com>
2016-05-18 10:32:07 -0400

    init

---
+++ chaos_method.py
@@ -0,0 +1,113 @@
+#try method corresponding to rahul's May 2nd email
+
+import numpy as np
+import local_library as ll
+import scipy.linalg as spl
+import time
+import cProfile
+import random
+import sys
+sys.path.append("/home/sgeraedt/TenPy/")
+from algorithms.linalg import np_conserved as npc
+np.set_printoptions(suppress=True,linewidth=150,precision=5,threshold=20000)
+
+
+def make_operators(operators,v2,Hclass):
+	Op_basis_size=operators.shape[0]
+	Op=[]
+	for j in range(Op_basis_size): Op.append(Hclass.project(operators[j],v2))
+	K=np.zeros((Op_basis_size,Op_basis_size))
+	for m in range(Op_basis_size):
+		for n in range(Op_basis_size):
+			if(Hclass.conserve):
+				K[n,m]=npc.tensordot(Op[n],Op[m],([0,1],[0,1])).real
+			else:
+				K[n,m]=np.tensordot(Op[n],Op[m],([0,1],[0,1])).real
+	
+	for m in range(Op_basis_size):
+		K[:,m]=K[:,m]
+	kw,kv=spl.eigh(K)
+
+	#make Op a set of orthogonal vectors that might not be local
+	
+#	for j in range(Op_basis_size): Op[j]=Hclass.to_ndarray(Op[j])
+	Op=np.array(Op)
+	Op=np.tensordot(kv,Op,([0],[0]))
+#	print Op
+	
+	return Op,kw[-1]
+
+kwfile=open("kw","w")
+
+Ns=[6,8,10,12]
+h=float(sys.argv[1])
+ROD=40
+#t=time.clock()
+print_times=True
+
+for N in Ns:
+	if(print_times): t=time.clock()
+	Hclass=ll.Hamiltonian(N,pbc=True,conserve=True,level=2)
+	if(print_times): print "time to init",time.clock()-t
+	Ntruncs=[2**(N-5),2**(N-4),2**(N-3),2**(N-2)]
+	#Ntruncs=[2**(N-2)]
+	Mout=np.zeros([len(Ntruncs),N])
+	newMout=np.zeros([len(Ntruncs),N])
+#	Mout=np.zeros([ROD,N])
+	failures=np.zeros(len(Ntruncs))
+
+	for seed in range(ROD):
+		if(print_times): t=time.clock()
+		Hclass.MPO_construction(h,seed)
+		if(print_times): print "time to make and solve H",time.clock()-t
+
+#		if(Hclass.conserve): v=Hclass.to_ndarray(Hclass.v)
+#		v=Hclass.v
+
+		for t_ind,Ntrunc in enumerate(Ntruncs):
+			if(print_times): t=time.clock()
+			try:
+				v2=Hclass.truncate(Hclass.v.copy(),Ntrunc)
+			except:
+				failures[t_ind]+=1
+				
+			if(print_times): print "time to truncate",time.clock()-t	
+	#		v2=np.dot(v2,v2.transpose())
+#			if(Hclass.conserve):
+#				q_flat=ll.make_q_flat(N)
+#				perm,v2= npc.array.from_ndarray_flat(v2,[q_flat,q_flat],bunch=True,sort=True,q_conj=[1,-1])
+			M=np.zeros([N,N])
+			M2=np.zeros([N,N])
+			best_ops=[]
+			for i in range(N):
+				if(print_times): t=time.clock()
+				tempops,kw=make_operators(Hclass.twobody_ops[i],v2,Hclass)
+				print >>kwfile,N,Ntrunc,i,kw
+				best_ops.append(tempops[-1])
+				if(print_times): print "time to find the best operators",time.clock()-t
+	#			print tempops
+
+			best_ops=np.array(best_ops)
+			diagops=[np.diagonal(best_op.to_ndarray()) for best_op in best_ops]
+			#compute the Mij
+			if(print_times): ti=time.clock()
+		
+			for i in range(N):
+				for j in range(N): 
+					if(Hclass.conserve):
+						M[i,j-i]=M[i,j-i]+npc.tensordot(best_ops[i],best_ops[j],[(0,1),(1,0)]).real
+						M2[i,j-i]=M2[i,j-i]+np.sum(np.diagonal(best_ops[i].to_ndarray())*np.diagonal(best_ops[j].to_ndarray()))
+						#M2[i,j-i]+=np.dot(diagops[i],diagops[j])
+					else:
+						M[i,j-i]=M[i,j-i]+np.tensordot(best_ops[i],best_ops[j],[(0,1),(1,0)]).real
+						M2[i,j-i]=M2[i,j-i]+np.sum(np.diagonal(best_ops[i])*np.diagonal(best_ops[j]))
+			Mout[t_ind,:]=Mout[t_ind,:]+np.mean(abs(M),axis=0)
+			newMout[t_ind,:]=newMout[t_ind,:]+np.mean(abs(M2),axis=0)
+			if(print_times): print "time to get the Ms",time.clock()-ti
+	for t_ind,failure in enumerate(failures):
+		Mout[t_ind,:]=Mout[t_ind,:]/(ROD-failure)
+		newMout[t_ind,:]/=(ROD-failure)
+	print "failures at N=",":",failures	
+	np.savetxt("M"+str(N),Mout.transpose())
+	np.savetxt("newM"+str(N),newMout.transpose())
+kwfile.close()	
---
+++ local_library.py
@@ -0,0 +1,282 @@
+#this code optimizes a local lbit to make it commute with the Hamiltonian
+import numpy as np
+import scipy.linalg as spl
+from scipy.optimize import minimize
+import random
+import time
+import sys
+sys.path.append("/home/sgeraedt/TenPy/")
+from algorithms.linalg import np_conserved as npc
+np.set_printoptions(suppress=True,linewidth=150,precision=8)
+
+#define pauli matrices
+
+class Hamiltonian:
+	def __init__(self,N,pbc=True,conserve=True,level=2):
+		self.N=N
+		self.pbc=pbc
+		self.conserve=conserve
+#		self.H_onebody=[]
+#		self.H_twobody=np.zeros([pow(2,N),pow(2,N)],dtype=np.float)
+
+		self.sx=sx=np.zeros([2,2],dtype=np.float)
+		sx[0,1]=1; sx[1,0]=1;
+		self.sy=sy=np.zeros([2,2],dtype=np.complex)
+		sy[0,1]=1J; sy[1,0]=-1J;
+		self.sz=sz=np.zeros([2,2],dtype=np.float)
+		sz[0,0]=1; sz[1,1]=-1;
+		self.Id=Id=np.eye(2,dtype=np.float)
+		self.sp=sp=np.zeros([2,2],dtype=np.float)
+		sp[0,1]=1;
+		self.sm=sm=np.zeros([2,2],dtype=np.float)
+		sm[1,0]=1;
+
+		#make pbc term
+		self.H_pbc=np.zeros([pow(2,N),pow(2,N)],dtype=np.float)
+		t=time.clock()
+		if(pbc):
+			op1=[0.25*sz,0.5*sp,0.5*sm]
+			op2=[sz,sm,sp]
+
+			link=np.zeros((1,2,2,1))
+			link[0,0,0,0]=link[0,1,1,0]=1
+			full_link=link
+			for j in range(N-3):
+				full_link=np.tensordot(full_link,link,([-1],[0]))
+			for i,op in enumerate(op1):
+				out=op.reshape((2,2,1))
+				out=np.tensordot(out,full_link,([-1],[0]))
+				out=np.tensordot(out,op2[i].reshape((1,2,2)),([-1],[0]))
+				out=out.transpose(range(0,2*N,2)+range(1,2*N,2))
+				out=out.reshape([2**N,2**N])
+
+				self.H_pbc+=out	
+			print self.H_pbc	
+		print "time to make pbc",time.clock()-t
+		t=time.clock()
+#		if(pbc):
+#			self.twobody_pbc=[]
+#			for j,op in enumerate([self.sx,self.sy,self.sz]):
+#				temp=0.25*op;
+#				for i in range(self.N-2):
+#					temp=spl.kron(temp,self.Id)
+#				temp=spl.kron(temp,op)
+#				self.H_pbc=self.H_pbc+temp.real
+#				self.twobody_pbc.append(temp.real)
+
+		self.twobody_ops=[]
+		for i in range(N): self.twobody_ops.append(self.make_twobody(i,level))
+		print "time to make twobody",time.clock()-t
+
+	def MPO_construction(self,h,seed=0,rans=None,alphax=0):
+		t=time.clock()
+		if rans is None:
+			random.seed(seed)
+			rans=[]
+			ransx=[]
+			for i in range(self.N): rans.append(2*(random.random()-1))
+			for i in range(self.N): ransx.append(2*(random.random()-1))
+		else: ransx=rans
+		
+		first=np.zeros((2,2,5),dtype=float)
+		last=np.zeros((2,2,5),dtype=float)
+		first[:,:,0]=self.Id
+		first[:,:,1]=0.25*self.sz
+		first[:,:,2]=0.5*self.sp
+		first[:,:,3]=0.5*self.sm
+		first[:,:,4]=0.5*h*(self.sz*rans[0]+alphax*self.sx*ransx[0])
+
+		last[:,:,0]=0.5*h*(self.sz*rans[-1]+alphax*self.sx*ransx[-1])
+		last[:,:,1]=self.sz
+		last[:,:,2]=self.sm
+		last[:,:,3]=self.sp
+		last[:,:,4]=self.Id
+		
+		out=first
+		for i in range(1,self.N-1):
+			mid=np.zeros((2,2,5,5),dtype=float)
+			
+			mid[:,:,0,0]=self.Id
+			mid[:,:,0,1]=0.25*self.sz
+			mid[:,:,0,2]=0.5*self.sp
+			mid[:,:,0,3]=0.5*self.sm
+			mid[:,:,0,4]=0.5*h*(self.sz*rans[i]+alphax*self.sx*ransx[i])
+			mid[:,:,1,4]=self.sz
+			mid[:,:,2,4]=self.sm
+			mid[:,:,3,4]=self.sp
+			mid[:,:,4,4]=self.Id
+			
+			out=np.tensordot(mid,out,([2],[2]))
+
+		
+		out=np.tensordot(last,out,([2],[2]))
+		out=out.transpose(range(0,2*self.N,2)+range(1,2*self.N,2))
+		out=out.reshape((2**self.N,2**self.N))
+
+		if(self.pbc): out=out+self.H_pbc
+		
+		if(self.conserve):
+			q_flat=make_q_flat(self.N)
+			self.perm,H= npc.array.from_ndarray_flat(out,[q_flat,q_flat],bunch=True,sort=True,q_conj=[1,-1])
+			self.w,self.v=npc.eigh(H)
+			
+			self.energy_keys=np.argsort(self.w)
+			self.energy_keys=np.argsort(self.energy_keys)
+		else:
+			self.w,self.v=spl.eigh(out)
+			
+	def to_ndarray(self,x):
+		v=x.to_ndarray()
+		if self.perm is not None: 
+			keys=np.argsort(self.perm[0])
+			v=v[keys,:]
+			v=v[:,keys]
+		return v
+	
+	def make_twobody(self,i,level):
+		N=self.N
+		Id=np.zeros((2,2,1,1))
+		Id[0,0,0,0]=Id[1,1,0,0]=1/np.sqrt(2)
+		Sz=np.zeros((2,2,1,1))
+		Sz[0,0,0,0]=1/np.sqrt(2); Sz[1,1,0,0]=-1/np.sqrt(2)
+		Sp=np.zeros((2,2,1,1))
+		Sp[0,1,0,0]=1
+		Sm=np.zeros((2,2,1,1))
+		Sm[1,0,0,0]=1
+		Sx=np.zeros((2,2,1,1))
+		Sx[:,:,0,0]=self.sx/np.sqrt(2)
+		Sy=np.zeros((2,2,1,1),dtype=complex)
+		Sy[:,:,0,0]=self.sy/np.sqrt(2)
+
+		out=[]
+		#level 1
+		out.append(self.assemble([(i,Sz)]))
+#		if(not self.conserve):
+#			out.append(self.assemble([(i,Sx)]))
+#			out.append(self.assemble([(i,Sy)]))
+		#level 2
+		if(level>=2):
+			if(self.conserve):
+				out.append(self.assemble([((i+1)%N,Sz)]))
+				out.append(self.assemble([(i,Sz),((i+1)%N,Sz)]))
+				out.append(  (self.assemble([(i,Sm),((i+1)%N,Sp)])+self.assemble([(i,Sp),((i+1)%N,Sm)]))/np.sqrt(2)  )
+			else:
+				sops=[Sx,Sy,Sz]
+				for op1 in sops:
+					out.append(self.assemble([((i+1)%N,op1)]))
+					for op2 in sops:
+						out.append(self.assemble([(i,op1),((i+1)%N,op2)]))
+		#level 3
+		if(level>=3):
+			out.append(self.assemble([((i-1)%N,Sz)]))		
+			out.append(self.assemble([(i,Sz),((i-1)%N,Sz)]))
+			out.append(  (self.assemble([(i,Sm),((i-1)%N,Sp)])+self.assemble([(i,Sp),((i-1)%N,Sm)]))/np.sqrt(2)  )
+		#level 4
+		if(level>=4):
+			out.append(self.assemble([((i-1)%N,Sz),((i+1)%N,Sz)]))
+			out.append(  (self.assemble([(i+1,Sm),((i-1)%N,Sp)])+self.assemble([(i+1,Sp),((i-1)%N,Sm)]))/np.sqrt(2)  )
+		#level 5
+		if(level>=5):
+			out.append(self.assemble([ ((i-1)%N,Sz),(i,Sz),((i+1)%N,Sz)   ]))
+			out.append( (self.assemble([ ((i-1)%N,Sz),(i,Sp),((i+1)%N,Sm)   ])+self.assemble([ ((i-1)%N,Sz),(i,Sm),((i+1)%N,Sp)]))/np.sqrt(2))
+			out.append( (self.assemble([ ((i-1)%N,Sp),(i,Sz),((i+1)%N,Sm)   ])+self.assemble([ ((i-1)%N,Sm),(i,Sz),((i+1)%N,Sp)]))/np.sqrt(2))
+			out.append( (self.assemble([ ((i-1)%N,Sp),(i,Sm),((i+1)%N,Sz)   ])+self.assemble([ ((i-1)%N,Sm),(i,Sp),((i+1)%N,Sz)])) /np.sqrt(2))
+		return np.array(out)
+		
+	def assemble(self,ops):
+		N=self.N
+		Id=np.zeros((2,2,1,1))
+		Id[0,0,0,0]=Id[1,1,0,0]=1/np.sqrt(2)
+		sites,ops=zip(*ops)
+		temp=np.ones((1))
+		for i in range(N):
+			try:
+				k=sites.index(i)
+				temp=np.tensordot(temp,ops[k],([-1],[2]))
+			except ValueError:
+				temp=np.tensordot(temp,Id,([-1],[2]))
+				
+		temp=temp.reshape([2]*(2*self.N))
+		temp=temp.transpose(range(0,2*self.N,2)+range(1,2*self.N,2))
+		temp=temp.reshape([2**self.N]*2)
+		if(self.conserve):
+			q_flat=make_q_flat(self.N)
+			perm,temp= npc.array.from_ndarray_flat(temp,[q_flat,q_flat],bunch=True,sort=True,q_conj=[1,-1])
+		return temp
+			
+	def project(self,op,v):
+		if(self.conserve):
+			vtemp=v.shallow_copy()
+			vtemp.q_conj=np.array([-1,1])
+			return npc.tensordot(npc.tensordot(vtemp,op,([0],[0])),v,([1],[0]))
+		else:
+			return np.tensordot(np.tensordot(v,op,([0],[0])),v,([1],[0]))
+
+	def truncate(self,v,Ntrunc):
+		#decide which indices to project out. Here I'll go with the indices in the middle
+#		start=v.shape[1]//2-Ntrunc//2
+#		if(Ntrunc%2): start=start-1
+#		end=v.shape[1]//2+Ntrunc//2
+		start=v.shape[1]-Ntrunc
+		end=v.shape[1]
+#		print start,end,Ntrunc
+#		print self.energy_keys
+#		print v.charge
+		nonempty_sectors=[]
+		if self.conserve:
+			qind_counter=0
+			otheraxis_counter=0
+			v.q_ind[1]=v.q_ind[1].copy()
+			newdat=[]; newqdat=[]
+			for charge,dat in enumerate(v.dat):
+#				print np.where(self.energy_keys[v.q_ind[1][charge,0]:v.q_ind[1][charge,1]]  <start)
+#				print np.where(self.energy_keys[v.q_ind[1][charge,0]:v.q_ind[1][charge,1]]  >=end)
+				temp=v.dat[charge][:,np.concatenate([np.where(self.energy_keys[v.q_ind[1][charge,0]:v.q_ind[1][charge,1]]  <start)[0],
+					np.where(self.energy_keys[v.q_ind[1][charge,0]:v.q_ind[1][charge,1]]  >=end)[0]])]
+				v.q_ind[1][charge,0]=qind_counter
+				qind_counter=temp.shape[1]+qind_counter
+				v.q_ind[1][charge,1]=qind_counter
+				if temp.shape[1]!=0: 
+					nonempty_sectors.append(charge)
+					newdat.append(temp)
+					newqdat.append(v.q_dat[charge,:])
+				else: otheraxis_counter=otheraxis_counter+v.dat[charge].shape[0]
+					
+
+				#fix the phase				
+				for i in range(v.dat[charge].shape[1]):
+					if v.dat[charge][np.argmax(abs(v.dat[charge][:,i])),i]<0: v.dat[charge][:,i]=v.dat[charge][:,i]*-1
+					
+			#eliminate charge sectors that now have no states
+			nonempty_sectors=np.array(nonempty_sectors)
+			v.q_ind[1]=v.q_ind[1][nonempty_sectors,:]
+			v.dat=newdat
+			v.q_dat=np.array(newqdat)
+			v.shape[1]=qind_counter
+						
+#			print v.q_ind[0]		
+#			print v.q_ind[1]
+#			print v.q_dat
+			v.check_sanity()
+			return v			
+		else:
+			for i in range(v.shape[1]):
+				if v[np.argmax(abs(v[:,i])),i]<0: v[:,i]=v[:,i]*-1
+			return np.hstack([v[:,:start],v[:,end:]])
+		
+	def chandranize(self,op,v):
+		if self.conserve :
+			raise NotImplementedError
+		else:
+			out=np.zeros(v.shape[1])
+			for i in range(v.shape[1]):
+				print np.tensordot(np.tensordot(v[:,i],op,([0],[0])),v[:,i],([0],[0]))
+				out[i]=np.tensordot(np.tensordot(v[:,i],op,([0],[0])),v[:,i],([0],[0])).real
+			out=np.diag(out)
+			return out
+def make_q_flat(N):
+	q_flat=[]
+	for i in range(pow(2,N)):
+		q_flat.append([bin(i).count("1")])
+	return np.array(q_flat)
+
